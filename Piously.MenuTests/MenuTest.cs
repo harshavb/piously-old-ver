7b06c877b243625cf86bc6489c6803513b6ff828
Ben Plate <Ben@Chris-iPhone.local>
2020-01-28 11:12:50 -0700

    Added bulge + bounce text

--- Piously.MenuTests/MenuTest.cs
+++ Piously.MenuTests/MenuTest.cs
@@ -17,12 +17,27 @@ namespace Piously.MenuTests
     [TestFixture]
     public class MenuTest : TestScene
     {
-        Container con;
-        byte changing = 0;
-        bool inc = true;
-        byte r = 255;
-        byte g = 0;
-        byte b = 0;
+
+        public Container con;
+
+        private class PiouslyText : SpriteText
+        {
+            public byte changing;
+            public bool inc;
+            public byte r;
+            public byte g;
+            public byte b;
+
+
+            public PiouslyText()
+            {
+                this.changing = 0;
+                this.inc = true;
+                this.r = 255;
+                this.g = 0;
+                this.b = 0;
+            }
+        }
 
         [BackgroundDependencyLoader]
         private void load()
@@ -34,6 +49,12 @@ namespace Piously.MenuTests
         public void bulgeText()
         {
             AddStep("Load bulging text", newBulgeText);
+        }
+
+        [Test]
+        public void bounceTest()
+        {
+            AddStep("Load bouncing text", newBounceText);
         }
 
         [Test]
@@ -51,7 +72,7 @@ namespace Piously.MenuTests
         }
         private void newBulgeText()
         {
-            SpriteText txt = new SpriteText
+            PiouslyText txt = new PiouslyText
             {
                 Text = "Piously",
                 Anchor = Anchor.Centre,
@@ -61,55 +82,74 @@ namespace Piously.MenuTests
                 Position = new Vector2(688, 334),
             };
 
-            Action<Drawable> bruh = updateText;
+            Action<Drawable> bruh = updateBulgeText;
             txt.OnUpdate += bruh;
 
             con.Add(txt);
             con.Show();
         }
 
-        private void updateText(Drawable obj)
+        private void newBounceText()
         {
-            SpriteText x = (SpriteText)obj;
+            PiouslyText txt = new PiouslyText
+            {
+                Text = "Piously",
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Colour = new Color4(255, 255, 255, 255),
+                Font = new FontUsage(null, 50),
+                Position = new Vector2(688, 334),
+            };
+
+            Action<Drawable> bruh = updateBounceText;
+            txt.OnUpdate += bruh;
+
+            con.Add(txt);
+            con.Show();
+        }
+
+        private void updateBulgeText(Drawable obj)
+        {
+            PiouslyText x = (PiouslyText)obj;
             obj.Rotation += 1.5f;
 
-            if (inc)
+            if (x.inc)
             {
                 x.Font = new FontUsage(null, x.Font.Size * 1.02f);
                 if(x.Font.Size >= 400)
                 {
-                    inc = false;
+                    x.inc = false;
                 }
             } else
             {
                 x.Font = new FontUsage(null, x.Font.Size / 1.02f);
                 if (x.Font.Size <= 12)
                 {
-                    inc = true;
+                    x.inc = true;
                 }
             }
 
-            switch(changing)
+            switch(x.changing)
             {
                 case 0:
-                    x.Colour = new Color4(r--, g++, b, 255);
-                    if(r == 0)
+                    x.Colour = new Color4(x.r--, x.g++, x.b, 255);
+                    if(x.r == 0)
                     {
-                        changing = 1;
+                        x.changing = 1;
                     }
                     break;
                 case 1:
-                    x.Colour = new Color4(r, g--, b++, 255);
-                    if(g == 0)
+                    x.Colour = new Color4(x.r, x.g--, x.b++, 255);
+                    if(x.g == 0)
                     {
-                        changing = 2;
+                        x.changing = 2;
                     }
                     break;
                 case 2:
-                    x.Colour = new Color4(r++, g, b--, 255);
-                    if(b == 0)
+                    x.Colour = new Color4(x.r++, x.g, x.b--, 255);
+                    if(x.b == 0)
                     {
-                        changing = 0;
+                        x.changing = 0;
                     }
                     break;
             }
@@ -117,6 +157,62 @@ namespace Piously.MenuTests
             if (x.Rotation >= 360)
             {
                 x.Rotation -= 360;
+            }
+        }
+
+        private void updateBounceText(Drawable obj)
+        {
+            PiouslyText x = (PiouslyText)obj;
+            switch (x.changing)
+            {
+                case 0:
+                    x.X += 1;
+                    x.Y += 1;
+                    if (x.X + x.Width / 2 >= this.LayoutRectangle.Width - con.ToParentSpace(new Vector2(0, 0)).X)
+                    {
+                        x.changing = 3;
+                    }
+                    else if (x.Y + x.Height / 2 >= this.LayoutRectangle.Height)
+                    {
+                        x.changing = 1;
+                    }
+                    break;
+                case 1:
+                    x.X += 1;
+                    x.Y -= 1;
+                    if (x.X + x.Width / 2 >= this.LayoutRectangle.Width - con.ToParentSpace(new Vector2(0, 0)).X)
+                    {
+                        x.changing = 2;
+                    }
+                    else if (x.Y - x.Height / 2 <= 0)
+                    {
+                        x.changing = 0;
+                    }
+                    break;
+                case 2:
+                    x.X -= 1;
+                    x.Y -= 1;
+                    if (x.X - x.Width / 2 <= 0)
+                    {
+                        x.changing = 1;
+                    }
+                    else if (x.Y - x.Height / 2 <= 0)
+                    {
+                        x.changing = 3;
+                    }
+                    break;
+                case 3:
+                    x.X -= 1;
+                    x.Y += 1;
+                    if (x.X - x.Width / 2 < 0)
+                    {
+                        x.changing = 0;
+                    }
+                    else if (x.Y + x.Height / 2 >= this.LayoutRectangle.Height)
+                    {
+                        x.changing = 2;
+                    }
+                    break;
             }
         }
     }
