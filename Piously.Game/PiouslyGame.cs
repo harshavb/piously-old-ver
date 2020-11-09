4de5b90a1063f90c52764c227d2018de2ad74e00
ben9583 <bplate9583@gmail.com>
2020-11-09 10:08:01 -0700

    Update and remove unused dependencies

--- Piously.Game/PiouslyGame.cs
+++ Piously.Game/PiouslyGame.cs
@@ -2,30 +2,21 @@
 using osu.Framework.Allocation;
 using osu.Framework.Logging;
 using osu.Framework.Screens;
-using osu.Framework.Graphics;
-using Piously.Game.Graphics.Primitives;
-using Piously.Game.Graphics.Shapes;
 using Piously.Game.Input.Bindings;
 using Piously.Game.Screens.Menu;
-using osuTK;
 using LogLevel = osu.Framework.Logging.LogLevel;
-using osu.Framework.Graphics.Textures;
-using osu.Framework.Graphics.Sprites;
 
 namespace Piously.Game
 {
     //The actual game, specifically loads the UI
     public class PiouslyGame : PiouslyGameBase
     {
-        private ScreenStack piouslyMenuScreenStack;
-        private TestScreen testScreen;
-        private ScreenStack piouslyMenuScreenStack2;
-        private TestScreen2 testScreen2;
         private ScreenStack mainMenuScreenStack;
         private MainMenu mainMenu;
-
-        private BackgroundScreen background;
         private ScreenStack backgroundStack;
+        private BackgroundScreen background;
+        private ScreenStack menuButtonsStack;
+
 
         [BackgroundDependencyLoader]
         private void load()
@@ -37,8 +28,6 @@ namespace Piously.Game
             }
 
             backgroundStack = new ScreenStack();
-            piouslyMenuScreenStack = new ScreenStack();
-            piouslyMenuScreenStack2 = new ScreenStack();
             mainMenuScreenStack = new ScreenStack();
 
             AddInternal(new PiouslyKeyBindingContainer
@@ -46,8 +35,6 @@ namespace Piously.Game
                 Children = new[]
                 {
                     backgroundStack,
-                    piouslyMenuScreenStack,
-                    piouslyMenuScreenStack2,
                     mainMenuScreenStack,
                 }
             });
@@ -58,36 +45,15 @@ namespace Piously.Game
             base.LoadComplete();
 
             background = new BackgroundScreen();
-            //testScreen = new TestScreen();
-            //testScreen2 = new TestScreen2();
             mainMenu = new MainMenu();
 
             backgroundStack.Push(background);
-            //piouslyMenuScreenStack.Push(testScreen);
-            //piouslyMenuScreenStack2.Push(testScreen2);
             mainMenuScreenStack.Push(mainMenu);
         }
 
         protected override void Update()
         {
             base.Update();
-            //testScreen.rotateHexagon();
-            //testScreen2.rotateText();
         }
-        
-        //THIS WORKS FINE
-        /*protected override bool OnKeyDown(KeyDownEvent e)
-        {
-            if (e.Key == Key.A)
-                Logger.Log("Detected A");
-            if(e.Key == Key.F)
-                Logger.Log("Detected F");
-            if (e.Key == Key.ControlLeft)
-                Logger.Log("Detected ControlLeft");
-
-            return base.OnKeyDown(e);
-        }*/
-
-        //THIS DOES NOT
     }
 }
