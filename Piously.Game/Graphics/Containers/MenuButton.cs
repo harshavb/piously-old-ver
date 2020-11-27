d5360fc773d78523005702f7bea928397df36704
harshavb <48032800+harshavb@users.noreply.github.com>
2020-11-27 06:52:39 -0700

    Comment a possible fix for hover logic

--- Piously.Game/Graphics/Containers/MenuButton.cs
+++ Piously.Game/Graphics/Containers/MenuButton.cs
@@ -2,6 +2,7 @@
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
+using osuTK;
 using Piously.Game.Graphics.Sprites;
 
 namespace Piously.Game.Graphics.Containers
@@ -12,6 +13,9 @@ namespace Piously.Game.Graphics.Containers
         public MenuLogo parentLogo;
         public MenuButtonSprite menuButtonSprite { get; private set; }
         //public Action clickAction;
+        
+        //Possible fix for hover logic?
+        //public override bool ReceivePositionalInputAt(Vector2 screenSpacePos) => menuButtonSprite.ReceivePositionalInputAt(screenSpacePos);
 
         [BackgroundDependencyLoader]
         private void load()
