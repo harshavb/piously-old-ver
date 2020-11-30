b40efbcf907bffb6190881322c24b1469bb7572f
harshavb <48032800+harshavb@users.noreply.github.com>
2020-11-30 12:39:56 -0700

    Unignore previously ignored files?

---
+++ Piously.Game/Overlays/Settings/Sections/Debug/GeneralSettings.cs
@@ -0,0 +1,29 @@
+﻿using osu.Framework.Allocation;
+using osu.Framework.Configuration;
+using osu.Framework.Graphics;
+
+namespace Piously.Game.Overlays.Settings.Sections.Debug
+{
+    public class GeneralSettings : SettingsSubsection
+    {
+        protected override string Header => "General";
+
+        [BackgroundDependencyLoader]
+        private void load(FrameworkDebugConfigManager config, FrameworkConfigManager frameworkConfig)
+        {
+            Children = new Drawable[]
+            {
+                new SettingsCheckbox
+                {
+                    LabelText = "Show log overlay",
+                    Current = frameworkConfig.GetBindable<bool>(FrameworkSetting.ShowLogOverlay)
+                },
+                new SettingsCheckbox
+                {
+                    LabelText = "Bypass front-to-back render pass",
+                    Current = config.GetBindable<bool>(DebugSetting.BypassFrontToBackPass)
+                }
+            };
+        }
+    }
+}
---
+++ Piously.Game/Overlays/Settings/Sections/Debug/MemorySettings.cs
@@ -0,0 +1,25 @@
+﻿using osu.Framework.Allocation;
+using osu.Framework.Configuration;
+using osu.Framework.Graphics;
+using osu.Framework.Platform;
+
+namespace Piously.Game.Overlays.Settings.Sections.Debug
+{
+    public class MemorySettings : SettingsSubsection
+    {
+        protected override string Header => "Memory";
+
+        [BackgroundDependencyLoader]
+        private void load(FrameworkDebugConfigManager config, GameHost host)
+        {
+            Children = new Drawable[]
+            {
+                new SettingsButton
+                {
+                    Text = "Clear all caches",
+                    Action = host.Collect
+                },
+            };
+        }
+    }
+}
