431eed8129cac06cc993c31ce685ee1c23f6aa1e
ben9583 <bplate9583@gmail.com>
2020-12-12 13:49:35 -0700

    Add StartButton

--- Piously.Game/Graphics/Containers/LocalGame/CreateGame/StartButtonContainer.cs
+++ Piously.Game/Graphics/Containers/LocalGame/CreateGame/StartButtonContainer.cs
@@ -1,11 +1,52 @@
-﻿using osu.Framework.Graphics.Containers;
+﻿using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
+using Piously.Game.Graphics.UserInterface;
+using osuTK;
 
 namespace Piously.Game.Graphics.Containers.LocalGame.CreateGame
 {
-    public class StartButtonContainer : Container
+    public class StartButtonContainer : CircularContainer
     {
-        public StartButtonContainer()
+        [BackgroundDependencyLoader]
+        private void load()
         {
+            Anchor = Anchor.BottomCentre;
+            Origin = Anchor.BottomCentre;
+            RelativeSizeAxes = Axes.Both;
+            RelativePositionAxes = Axes.Both;
+            Size = new Vector2(0.6f, 0.1f);
+            Position = new Vector2(0f, -0.025f);
+            Masking = true;
+
+            Children = new Drawable[]
+            {
+                new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Size = new Vector2(1f),
+                    Colour = PiouslyColour.PiouslyYellow,
+                },
+
+                new SpriteText
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Font = new FontUsage("InkFree", 40, "Bold", false, false),
+                    Text = "START",
+                },
+
+                new PiouslyButton
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    RelativeSizeAxes = Axes.Both,
+                    Size = new Vector2(1f),
+                    Colour = new Colour4(0, 0, 0, 0),
+                }
+            };
         }
     }
 }
