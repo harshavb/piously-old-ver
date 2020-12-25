e2056c46823911544c3f1d73e722c4a668bf7b37
harshavb <48032800+harshavb@users.noreply.github.com>
2020-12-24 23:22:12 -0700

    Minor changes to HexagonGroup.cs

--- Piously.Game/Graphics/Containers/HexagonGroup.cs
+++ Piously.Game/Graphics/Containers/HexagonGroup.cs
@@ -7,12 +7,13 @@ namespace Piously.Game.Graphics.Containers
     public class HexagonGroup : Container<HexagonalContainer>
     {
         //offset variable
-        //2D array representing hexagons
+        //2D array representing hexagons, the length of each dimension representing how many hexagons should exist.
+        //We want the amount of hexagons to not affect the size of the total HexagonGroup
 
         [BackgroundDependencyLoader]
         private void load()
         {
-            AutoSizeAxes = Axes.Both;
+            RelativeSizeAxes = Axes.Both;
 
             Children = new HexagonalContainer[]
             {
