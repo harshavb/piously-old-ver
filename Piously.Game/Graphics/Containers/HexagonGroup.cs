c4c10296b7ccc390ce9d056fc9df8f5c2bfb02c7
harshavb <48032800+harshavb@users.noreply.github.com>
2020-12-24 23:18:29 -0700

    Implement basic structure of HexagonGroup.cs

---
+++ Piously.Game/Graphics/Containers/HexagonGroup.cs
@@ -0,0 +1,26 @@
+﻿using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+
+namespace Piously.Game.Graphics.Containers
+{
+    public class HexagonGroup : Container<HexagonalContainer>
+    {
+        //offset variable
+        //2D array representing hexagons
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            AutoSizeAxes = Axes.Both;
+
+            Children = new HexagonalContainer[]
+            {
+                new HexagonalContainer
+                {
+
+                }
+            };
+        }
+    }
+}
