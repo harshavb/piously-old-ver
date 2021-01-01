7931ea35af0530594710a8a9ab8f520c06194d93
harshavb <48032800+harshavb@users.noreply.github.com>
2021-01-01 01:15:57 -0700

    Make start and resume buttons inside of LocalGameContainer hoverable

--- Piously.Game/Graphics/Containers/LocalGame/CreateGame/StartButtonContainer.cs
+++ Piously.Game/Graphics/Containers/LocalGame/CreateGame/StartButtonContainer.cs
@@ -1,16 +1,21 @@
 ﻿using osu.Framework.Allocation;
+using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
+using osu.Framework.Input.Events;
 using Piously.Game.Graphics.Backgrounds;
 using Piously.Game.Graphics.UserInterface;
 using osuTK;
+using osuTK.Graphics;
 
 namespace Piously.Game.Graphics.Containers.LocalGame.CreateGame
 {
     public class StartButtonContainer : CircularContainer
     {
+        protected Box Hover;
+
         [BackgroundDependencyLoader]
         private void load(PiouslyColour colour)
         {
@@ -24,13 +29,22 @@ namespace Piously.Game.Graphics.Containers.LocalGame.CreateGame
 
             Children = new Drawable[]
             {
+                Hover = new Box
+                {
+                        Alpha = 0,
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        RelativeSizeAxes = Axes.Both,
+                        Colour = Color4.White.Opacity(.1f),
+                        Blending = BlendingParameters.Additive,
+                        Depth = float.MinValue
+                },
                 new Box
                 {
                     RelativeSizeAxes = Axes.Both,
                     Size = new Vector2(1f),
                     Colour = colour.Yellow,
                 },
-
                 new Hexagons
                 {
                     HexagonScale = 2,
@@ -39,7 +53,6 @@ namespace Piously.Game.Graphics.Containers.LocalGame.CreateGame
                     RelativeSizeAxes = Axes.Both,
                     Size = new Vector2(1f, 3f),
                 },
-
                 new SpriteText
                 {
                     Anchor = Anchor.Centre,
@@ -47,7 +60,6 @@ namespace Piously.Game.Graphics.Containers.LocalGame.CreateGame
                     Font = new FontUsage("Aller", 40, "Bold", false, false),
                     Text = "START",
                 },
-
                 new PiouslyButton
                 {
                     Anchor = Anchor.Centre,
@@ -57,6 +69,20 @@ namespace Piously.Game.Graphics.Containers.LocalGame.CreateGame
                     Colour = new Colour4(0, 0, 0, 0),
                 }
             };
+        }
+
+        protected override bool OnHover(HoverEvent e)
+        {
+            Hover.FadeIn(200, Easing.OutQuint);
+
+            return base.OnHover(e);
+        }
+
+        protected override void OnHoverLost(HoverLostEvent e)
+        {
+            base.OnHoverLost(e);
+
+            Hover.FadeOut(300);
         }
     }
 }
